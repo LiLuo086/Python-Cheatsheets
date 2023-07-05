6006e6ed3ece1a89f028a57a6fd61aed78266c7d
Li Luo <LiLuo086@users.noreply.github.com>
2023-07-05 21:38:02 +0200

    add number_tipps.py
    https://docs.python.org/3/library/math.html

---
+++ number_tipps.py
@@ -0,0 +1,33 @@
+
+import math  # import Python math package
+
+# number types
+x = 3  # x is an integer
+x = 3.33  # x is a float
+x = 1+2j  # x is a complex number
+
+# arithmetic operators
+print(2 + 3)
+print(3 - 1)
+print(2 * 3)
+print(3 / 2)
+print(3 // 2)  # return the integer part of the division result
+print(3 % 2)  # return the remainder of the division result
+print(3 ** 2)  # 3 to the power of 2
+
+
+# augmented assignemnt operator
+y = 5
+y = y + 3
+print(y)
+y += 3
+print(y)
+
+
+# Python built-in functions and methods for numbers
+print(round(2.3))  # the rounded number
+print(abs(-1.5))  # the absolute value
+print(math.ceil(2.1))  # the ceilings of a number
+print(math.ceil(-2.1))
+
+# Document to Python math package https://docs.python.org/3/library/math.html
